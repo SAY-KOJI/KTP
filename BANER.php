3dc438d21fab1e97afb6f3af2bdee3d4fbddb42c
SAY-KOJI <56780682+SAY-KOJI@users.noreply.github.com>
2019-10-21 15:28:58 +0700

    Add files via upload

---
+++ BANER.php
@@ -0,0 +1,47 @@
+<?php
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function kkktp_banner(){
+  echo "\e[91;32m
+                          _
+              ---           / |
+              \   \        /  |
+               \    \     /   /
+                 \   \   |   /
+                   \  \  | /
+                  _ _\ \/ /
+                 /       <
+                / (@)     \
+               (           |
+                \..       /
+                   | ____/
+                  (V) ===
+                  (A)
+    ____   _       _____      _      ____
+   / ___| | |     | ____|    / \    |  _ \
+  | |     | |     |  _|     / _ \   | |_) |
+  | |___  | |___  | |___   / ___ \  |  _ <
+   \____| |_____| |_____| /_/   \_\ |_| \_\
+ 
+  \n";
+}
+?>
---
+++ README.md
@@ -0,0 +1,15 @@
+## KK & KTP Generator
+# Command Install For Termux / Linux (pc)
+```
+$ pkg install php
+$ pkg install git
+$ git clone https://github.com/SAY-KOJI/KTP
+$ cd KTP
+$ ls
+$ php kkktp.php
+```
+# Pembuat
+SAY-KOJI  
+
+# Spesial Thank
+indonesian Cyber | Team
